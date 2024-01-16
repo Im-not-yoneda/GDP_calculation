77476a232acc7011e4a70e2b3984098e0e2a4c5f
米田匠吾 <yoneda.shogo@gmail.com>
2024-01-16 17:59:29 +0900

    修正

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
   python_requires=">=3.6",
   entry_points = {
       'console_scripts': [
-          'imageComp = imageComp:main'
+          'imageComp = src/imageComp:main'
       ]
   },
 ) 
