fe0bc6a8f9249bf4109e81a441343a451c045d8b
米田匠吾 <yoneda.shogo@gmail.com>
2024-01-16 18:39:38 +0900

    バージョンの変更

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 setuptools.setup(
   name="imageComp",
-  version="0.0.2",
+  version="0.0.3",
   author="Yoneda",
   author_email="s2122053@stu.musashino-u.ac.jp",
   description="A small example package",
